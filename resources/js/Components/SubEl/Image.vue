ec850374c6d7ac5882d6eb72b615680bd676fa91
Aobo <aobozhang@gmail.com>
2024-10-31 08:54:46 +0800

    Fix route back

--- resources/js/Components/SubEl/Image.vue
+++ resources/js/Components/SubEl/Image.vue
@@ -4,6 +4,7 @@ import _ from 'lodash-es';
 import { watch } from 'vue';
 import { Link, router, useForm, usePage } from '@inertiajs/vue3';
 import { toBlob, toJpeg } from 'html-to-image';
+import { dataURItoBlob } from './func.js'
 
 const props = defineProps({
     item: {
@@ -158,32 +159,12 @@ const itemUpload = async (e) => {
 
     router.post(route('ads-item.store'), form, {
         forceFormData: true,
-        replace: true,
         onSuccess: (page) => {
             item.value.url = page.props.flash?.upload?.url;
         }
     });
 }
 
-function dataURItoBlob(dataURI) {
-    // convert base64/URLEncoded data component to raw binary data held in a string
-    var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
-
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-
-    return new Blob([ia], { type: mimeString });
-}
 
 const bgImage = new Image();
 
@@ -209,7 +190,6 @@ const saveJpeg = async () => {
 
         router.post(route('ads-item.store'), form, {
             forceFormData: true,
-            replace: true,
             onSuccess: (page) => {
                 item.value.url = page.props.flash?.upload?.url;
             }
--- resources/js/Components/SubEl/Main.vue
+++ resources/js/Components/SubEl/Main.vue
@@ -4,6 +4,7 @@ import _ from 'lodash-es';
 import { watch } from 'vue';
 import { Link, router, useForm, usePage } from '@inertiajs/vue3';
 import { toBlob, toJpeg } from 'html-to-image';
+import { dataURItoBlob } from './func.js'
 
 const props = defineProps({
     item: {
@@ -60,25 +61,6 @@ const onUrlChange = () => {
     bgImage.onload = saveJpeg;
 }
 
-function dataURItoBlob(dataURI) {
-    // convert base64/URLEncoded data component to raw binary data held in a string
-    var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
-
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-
-    return new Blob([ia], { type: mimeString });
-}
 
 const saveJpeg = async () => {
 
